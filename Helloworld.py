7094df2fbf523bf786d5e90cace21ca7aa4ab252
Pekpostalci <Osman.Pekpostalci@nielsen.com>
2018-11-24 22:47:55 +0300

    This is hello world file

---
+++ Helloworld.py
@@ -0,0 +1,9 @@
+employee=[
+    ["serkan","2010","kadıköy"],
+    ["ahmet","2008","bakırköy"],
+    ["buğra","2015","göztepe"],
+    ["merve","2010","üsküdar"]
+]
+for work in employee:
+    for kisi in work:
+        print("{0}".format(kisi))
